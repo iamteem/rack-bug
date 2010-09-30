4b48390511b760145673cdd9dd5a9ba2d5b98cd7
Tim Medina <iamteem@gmail.com>
2010-10-01 02:44:28 +0800

    Add alias_method_chain in spec_helper.

--- lib/rack/bug/panels/templates_panel/sinatra_extension.rb
+++ lib/rack/bug/panels/templates_panel/sinatra_extension.rb
@@ -1,6 +1,5 @@
 if defined?(Sinatra) && defined?(Sinatra::Templates)
   Sinatra::Templates.class_eval do
-
     [:erb, :erubis, :haml, :sass, :less, :builder].each do |meth|
       class_eval <<-END
         def #{ meth }_with_rack_bug(template, *args, &block)
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -78,3 +78,30 @@ Spec::Runner.configure do |config|
     get(uri, params, env, &block)
   end
 end
+
+
+unless Module.methods.include?(:alias_method_chain)
+  # Taken from active_support/core-ext
+  class Module
+    def alias_method_chain(target, feature)
+      # Strip out punctuation on predicates or bang methods since
+      # e.g. target?_without_feature is not a valid method name.
+      aliased_target, punctuation = target.to_s.sub(/([?!=])$/, ''), $1
+      yield(aliased_target, punctuation) if block_given?
+
+      with_method, without_method = "#{aliased_target}_with_#{feature}#{punctuation}", "#{aliased_target}_without_#{feature}#{punctuation}"
+
+      alias_method without_method, target
+      alias_method target, with_method
+
+      case
+        when public_method_defined?(without_method)
+          public target
+        when protected_method_defined?(without_method)
+          protected target
+        when private_method_defined?(without_method)
+          private target
+      end
+    end
+  end
+end
